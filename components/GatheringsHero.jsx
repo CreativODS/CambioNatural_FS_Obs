2a16c1bbfe881d3f7085c247b2f90f878fe84760
CreativODS <gaytancerezo.alberto@gmail.com>
2026-05-26 23:04:49 -0600

    herogatherings 07

--- components/GatheringsHero.jsx
+++ components/GatheringsHero.jsx
@@ -2,20 +2,8 @@
 
 export default function GatheringsHero({ children }) {
   return (
-    <section className="relative w-full min-h-[70vh] bg-[#0A6CFF] overflow-hidden flex items-center">
-      {/* Textura */}
-      <div className="absolute inset-0 pointer-events-none z-0">
-        <img
-          src="/assets/images/oceanos blanco.png"
-          alt=""
-          className="absolute top-1/2 right-[-18%] w-[30%] opacity-90"
-        />
-      </div>
-
-      {/* Contenedor de contenido: ahora solo actúa como marco */}
-      <div className="relative z-10 w-full max-w-[1200px] mx-auto px-6">
-        {children}
-      </div>
+    <section className="relative w-full bg-[#0A6CFF] py-20 overflow-hidden">
+      {children}
     </section>
   );
 }
