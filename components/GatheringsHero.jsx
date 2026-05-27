0b5abecd82958d5846b99f825da85f2fb1737070
CreativODS <gaytancerezo.alberto@gmail.com>
2026-05-26 21:52:46 -0600

    gathering06

--- components/GatheringsHero.jsx
+++ components/GatheringsHero.jsx
@@ -2,18 +2,18 @@
 
 export default function GatheringsHero({ children }) {
   return (
-    <section className="relative w-full min-h-[70vh] bg-[#0A6CFF] overflow-hidden flex items-center justify-center">
-      {/* Imagen de fondo (textura) */}
+    <section className="relative w-full min-h-[70vh] bg-[#0A6CFF] overflow-hidden flex items-center">
+      {/* Textura */}
       <div className="absolute inset-0 pointer-events-none z-0">
         <img
           src="/assets/images/oceanos blanco.png"
           alt=""
-          className="absolute top-1/3 -translate-y-1/2 right-[-18%] w-[38%] max-w-none opacity-90"
+          className="absolute top-1/2 right-[-18%] w-[30%] opacity-90"
         />
       </div>
 
       {/* Contenedor de contenido: ahora solo actúa como marco */}
-      <div className="relative z-10 w-full max-w-[760px] px-6 text-center text-white">
+      <div className="relative z-10 w-full max-w-[1200px] mx-auto px-6">
         {children}
       </div>
     </section>
