dfd5c2c1fd779545ef738f7ba07e3ace6aa7d01c
CreativODS <gaytancerezo.alberto@gmail.com>
2026-05-26 23:16:27 -0600

    newsletter conexition

--- components/Newsletter.js
+++ components/Newsletter.js
@@ -1,9 +1,7 @@
 // components/Newsletter.js
 export default function Newsletter() {
   return (
-    <div className="main-grid py-12">
-      {/* Columna 1 y 2: Información */}
-      <div className="bg-white p-16 rounded-xl shadow-lg w-full text-black">
+    <div className="bg-white p-16 rounded-xl shadow-lg text-black">
         <p className="text-sm uppercase tracking-wide">Join to Newsletter</p>
         <h2 className="font-dela text-3xl my-4 uppercase">Lets co-create together</h2>
         <p className="text-base">
@@ -14,7 +12,7 @@ export default function Newsletter() {
       {/* Columnas 3, 4 y 5: Imagen y formulario */}
       <div className="col-span-3 flex flex-col gap-4">
         {/* Imagen principal */}
-        <img src="/assets/images/newsletter-visual.png" alt="Newsletter" className="w-full max-h-[200px] object-contain" />
+        <img src="/assets/images/meta-crisis.png" alt="Newsletter" className="w-full max-h-[200px] object-contain" />
         
         {/* Formulario: Ocupa col 3 y 4, botón en col 5 */}
         <div className="grid grid-cols-3 gap-2">
