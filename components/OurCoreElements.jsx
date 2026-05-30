a47aad6ddb81c963abcdd98031a1f18dcde91450
CreativODS <gaytancerezo.alberto@gmail.com>
2026-05-30 16:43:27 -0600

    ourcoreelements

--- components/OurCoreElements.jsx
+++ components/OurCoreElements.jsx
@@ -1,68 +1,47 @@
-"use client";
-import React from "react";
-
-export default function OurCoreElements({ children }) {
+export default function OurCoreElements({ title, children }) {
   return (
-    <section className="w-full bg-white text-black py-40">
-      
-      {/* Título */}
-      <div className="main-grid w-full mb-32">
+    <section className="w-full bg-white text-black py-20 md:py-40">
+      {/* Título de la sección */}
+      <div className="main-grid w-full mb-20">
         <h2 className="col-span-5 font-dela text-3xl md:text-4xl uppercase tracking-normal">
-          Our Core Elements
+          {title}
         </h2>
       </div>
 
-      <div
-        className="
-          w-full 
-          flex 
-          flex-col 
-          gap-y-44
-
-          [&>div]:main-grid
-          [&>div]:w-full
-          [&>div]:items-center
-
-          {/* Texto ocupa columnas 1, 2 y 3 */}
-          [&>div:nth-child(odd)>div]:col-span-5
-          md:[&>div:nth-child(odd)>div]:col-start-1
-          md:[&>div:nth-child(odd)>div]:col-span-3
-
-          [&>div:nth-child(odd)>div:has(img)]:col-span-5
-          md:[&>div:nth-child(odd)>div:has(img)]:col-start-4
-          md:[&>div:nth-child(odd)>div:has(img)]:col-span-2
-
-          md:[&>div:nth-child(even)>div:has(img)]:col-start-1
-          md:[&>div:nth-child(even)>div:has(img)]:col-span-2
-          md:[&>div:nth-child(even)>div:has(img)]:row-start-1
-
-          md:[&>div:nth-child(even)>div:not(:has(img))]:col-start-3
-          md:[&>div:nth-child(even)>div:not(:has(img))]:col-span-3
-
-          [&>div_img]:w-full
-          [&>div_img]:aspect-[4/3]
-          [&>div_img]:object-cover
-          [&>div_img]:rounded-2xl
-          
-          {/* text */}
-          [&>div>div>span:first-child]:block
-          [&>div>div>span:first-child]:font-dela
-          [&>div>div>span:first-child]:text-2xl
-          md:[&>div>div>span:first-child]:text-3xl
-          [&>div>div>span:first-child]:text-black
-          [&>div>div>span:first-child]:leading-relaxed
-          [&>div>div>span:first-child]:mb-6
-
-          {/* Siguientes spans = Cuerpo en Font Inter */}
-          [&>div>div>span:not(:first-child)]:block
-          [&>div>div>span:not(:first-child)]:font-inter
-          [&>div>div>span:not(:first-child)]:text-3xl
-          md:[&>div>div>span:not(:first-child)]:text-4xl
-          [&>div>div>span:not(:first-child)]:leading-relaxed
-        "
-      >
+      {/* Contenedor de elementos */}
+      <div className="flex flex-col gap-y-20 md:gap-y-40">
         {children}
       </div>
     </section>
+  );
+}
+
+// Este es el sub-componente que usarás dentro de CoreElements
+export function CoreItem({ title, description, imageSrc, imageAlt, reverse = false }) {
+  return (
+    <div className="main-grid w-full items-center">
+      {/* Texto - Ocupa 3 columnas */}
+      <div 
+        className={`col-span-5 md:col-span-3 flex flex-col space-y-6 ${reverse ? 'md:col-start-3' : 'md:col-start-1'}`}
+      >
+        <h3 className="font-dela text-xl md:text-2xl tracking-normal leading-relaxed">
+          {title}
+        </h3>
+        <p className="font-inter text-2xl md:text-3xl leading-relaxed">
+          {description}
+        </p>
+      </div>
+
+      {/* Imagen */}
+      <div 
+        className={`col-span-5 md:col-span-2 aspect-[4/3] overflow-hidden rounded-2xl ${reverse ? 'md:col-start-1 md:row-start-1' : 'md:col-start-4'}`}
+      >
+        <img 
+          src={imageSrc} 
+          alt={imageAlt || title} 
+          className="w-full h-full object-cover"
+        />
+      </div>
+    </div>
   );
 }
