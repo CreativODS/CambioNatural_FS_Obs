35a2aa0b75740621ef33fa00a0a1f63718632d47
CreativODS <gaytancerezo.alberto@gmail.com>
2026-05-29 19:56:39 -0600

    WeAre  CoreElements Update

--- components/OurCoreElements.jsx
+++ components/OurCoreElements.jsx
@@ -1,57 +1,67 @@
 "use client";
+import React from "react";
 
 export default function OurCoreElements({ children }) {
   return (
-    <section className="w-full bg-white py-40">
-      <div className="container mx-auto px-6 max-w-6xl">
-
-        {/* Título sección */}
-        <h2 className="font-dela text-4xl font-black uppercase mb-32">
-          OUR CORE ELEMENTS
+    <section className="w-full bg-white text-black py-40">
+      
+      {/* Título */}
+      <div className="main-grid w-full mb-32">
+        <h2 className="col-span-5 font-dela text-3xl md:text-4xl uppercase tracking-normal">
+          Our Core Elements
         </h2>
+      </div>
 
-        {/* Lista */}
-        <div
-          className="
-            flex flex-col gap-44
+      <div
+        className="
+          w-full 
+          flex 
+          flex-col 
+          gap-y-44
 
-            /* Cada bloque */
-            [&>div]:grid
-            [&>div]:grid-cols-[320px_1fr]
-            [&>div]:gap-24
-            [&>div]:items-start
+          [&>div]:main-grid
+          [&>div]:w-full
+          [&>div]:items-center
 
-            /* Alternar composición */
-            [&>div:nth-child(even)]:grid-cols-[1fr_320px]
-            [&>div:nth-child(even)>img]:order-2
-            [&>div:nth-child(even)>div]:order-1
+          {/* Texto ocupa columnas 1, 2 y 3 */}
+          [&>div:nth-child(odd)>div]:col-span-5
+          md:[&>div:nth-child(odd)>div]:col-start-1
+          md:[&>div:nth-child(odd)>div]:col-span-3
 
-            /* IMÁGENES */
-            [&>div>img]:w-full
-            [&>div>img]:max-w-[420px]
-            [&>div>img]:h-auto
+          [&>div:nth-child(odd)>div:has(img)]:col-span-5
+          md:[&>div:nth-child(odd)>div:has(img)]:col-start-4
+          md:[&>div:nth-child(odd)>div:has(img)]:col-span-2
 
-            /* TEXTO */
-            [&>div>div]:mt-24
+          md:[&>div:nth-child(even)>div:has(img)]:col-start-1
+          md:[&>div:nth-child(even)>div:has(img)]:col-span-2
+          md:[&>div:nth-child(even)>div:has(img)]:row-start-1
 
-            /* Primer span = título */
-            [&>div>div>span:first-child]:block
-            [&>div>div>span:first-child]:font-dela
-            [&>div>div>span:first-child]:text-3xl
-            [&>div>div>span:first-child]:leading-tight
-            [&>div>div>span:first-child]:mb-6
+          md:[&>div:nth-child(even)>div:not(:has(img))]:col-start-3
+          md:[&>div:nth-child(even)>div:not(:has(img))]:col-span-3
 
-            /* Demás spans = cuerpo */
-            [&>div>div>span:not(:first-child)]:block
-            [&>div>div>span:not(:first-child)]:font-inter
-            [&>div>div>span:not(:first-child)]:text-2xl
-            [&>div>div>span:not(:first-child)]:text-gray-700
-            [&>div>div>span:not(:first-child)]:max-w-md
-          "
-        >
-          {children}
-        </div>
+          [&>div_img]:w-full
+          [&>div_img]:aspect-[4/3]
+          [&>div_img]:object-cover
+          [&>div_img]:rounded-2xl
+          
+          {/* text */}
+          [&>div>div>span:first-child]:block
+          [&>div>div>span:first-child]:font-dela
+          [&>div>div>span:first-child]:text-2xl
+          md:[&>div>div>span:first-child]:text-3xl
+          [&>div>div>span:first-child]:text-black
+          [&>div>div>span:first-child]:leading-relaxed
+          [&>div>div>span:first-child]:mb-6
 
+          {/* Siguientes spans = Cuerpo en Font Inter */}
+          [&>div>div>span:not(:first-child)]:block
+          [&>div>div>span:not(:first-child)]:font-inter
+          [&>div>div>span:not(:first-child)]:text-3xl
+          md:[&>div>div>span:not(:first-child)]:text-4xl
+          [&>div>div>span:not(:first-child)]:leading-relaxed
+        "
+      >
+        {children}
       </div>
     </section>
   );
